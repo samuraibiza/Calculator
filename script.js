d1a50ed966e89733e89b58a99cf5d66bf732df0c
Leonid Raldes <lmraldes@gmail.com>
2024-10-12 19:28:55 +0200

    v.0.1.1 simple working version

--- script.js
+++ script.js
@@ -1,42 +1,17 @@
 const calcScreen = document.querySelector(".calc-screen");
-const number = [0,1,2,3,4,5,6,7,8,9];
 
-let aInteger = number[1];
-let bInteger = number[2];
+const addValue = (calcInput) => {
+  calcScreen.value += calcInput;
+};
 
-const activeInteger = aInteger;
-
-const sum = () => {
-  sumResult = aInteger + bInteger;
-  calcScreen.innerHTML = sumResult;
+const sliceValue = () => {
+  calcScreen.value = calcScreen.value.slice(0, -1);
 };
 
 const setZero = () => {
-  aInteger = number[0];
-  calcScreen.innerHTML = aInteger;
+  calcScreen.value = "";
 };
 
-// const createChatMEssageElement = (message) => `
-// <div class="msg-placing-active">
-//   <div class="message primary-msg primary-bg">
-//     <div class="message-sender">${message.sender}</div>
-//     <div class="message-text">${message.text}</div>
-//     <div class="message-timestamp">${message.timeStamp}</div>
-//   </div>
-// </div>
-// `;
-
-// const sendMessage = () => {
-//   const timeStamp = new Date().toLocaleString("en-US", {
-//     hour: "numeric",
-//     minute: "numeric",
-//     hour12: true,
-//   });
-//   const message = {
-//     sender: "Leo",
-//     text: chatInput.value,
-//     timeStamp: timeStamp,
-//   };
-
-//   chatMessages.innerHTML += createChatMEssageElement(message);
-// };
+const result = () => {
+  calcScreen.value = eval(calcScreen.value);
+};
