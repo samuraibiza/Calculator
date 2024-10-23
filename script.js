b4cf9aff4a5d0e2d1dff60179d99ef88da006a5c
Leonid Raldes <lmraldes@gmail.com>
2024-10-23 21:53:21 +0200

    v.0.3.1 Full simple JS + keyEvents

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+const AddValueKeys = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "0",
+  ".",
+  "+",
+  "-",
+  "*",
+  "/",
+];
+
+window.addEventListener("keydown", (addValueKeyEvent) => {
+  if (AddValueKeys.includes(addValueKeyEvent.key)) {
+    calcScreen.value += addValueKeyEvent.key;
+  } else if (
+    addValueKeyEvent.key === "Backspace" ||
+    addValueKeyEvent.key === "Delete"
+  ) {
+    calcScreen.value = calcScreen.value.slice(0, -1);
+  } else if (
+    addValueKeyEvent.key === "Escape" ||
+    addValueKeyEvent.key === "c"
+  ) {
+    calcScreen.value = "";
+  } else if (addValueKeyEvent.key === "r") {
+    calcScreen.value = Math.sqrt(calcScreen.value);
+  } else if (addValueKeyEvent.key === "n") {
+    if (calcScreen.value[0] == "-") {
+      calcScreen.value = calcScreen.value.slice(1);
+    } else {
+      calcScreen.value = "-" + calcScreen.value;
+    }
+  } else if (addValueKeyEvent.key === " " || addValueKeyEvent.key === "Enter") {
+    calcScreen.value = eval(calcScreen.value);
+  } else {
+    return;
+  }
+});
+
 const calcScreen = document.querySelector(".calc-screen");
 
 const addValue = (calcInput) => {
@@ -17,7 +64,11 @@ const negateValue = () => {
 };
 
 const setZero = () => {
-  calcScreen.value = "-";
+  calcScreen.value = "";
+};
+
+const findRoot = () => {
+  calcScreen.value = Math.sqrt(calcScreen.value);
 };
 
 const result = () => {
